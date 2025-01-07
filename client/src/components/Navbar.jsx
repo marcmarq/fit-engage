56974fc74d42159eb5e446a1cf47ee7f39b7f5d8
simo <msMarquez@mcm.edu.ph>
2025-01-08 00:35:04 +0800

    added all the features for reset password

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
 
   return (
     <div className="w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0">
-      <img src={assets.logo} alt="" className="w-28 sm:w-32" />
+      <img src={assets.kiloKulture_Logo} alt="" className="w-18 sm:w-20" />
       {userData ? 
       <div>
         {userData.name[0].toUpperCase()}
--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -1,11 +1,21 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import { assets } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContent } from '../context/AppContext'
+import axios from 'axios'
+import { toast } from "react-toastify";
 
 const ResetPassword = () => {
 
+  const {backendUrl} = useContext(AppContent)
+  axios.defaults.withCredentials = true
+
   const navigate = useNavigate()
   const [email, setEmail] = useState('')
+  const [newPassword, setNewPassword] = useState('')
+  const [isEmailSent, setIsEmailSent] = useState('')
+  const [otp, setOtp] = useState(0)
+  const [isOtpSubmitted, setIsOtpSubmitted] = useState(false)
 
   const inputRefs = React.useRef([])
 
@@ -31,12 +41,44 @@ const ResetPassword = () => {
     })
   }
 
+  const onSubmitEmail = async (e)=>{
+    e.preventDefault();
+    try {
+      const {data} = await axios.post(backendUrl + '/api/auth/send-reset-otp', {email})
+      data.success ? toast.success(data.message) : toast.error(data.message)
+      data.success && setIsEmailSent(true)
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const onSubmitOTP = async (e)=>{
+    e.preventDefault()
+    const otpArray = inputRefs.current.map(e => e.value)
+    setOtp(otpArray.join(''))
+    setIsOtpSubmitted(true)
+  }
+
+  const onSubmitNewPassword = async (e)=> {
+    e.preventDefault();
+    try {
+      const {data} = await axios.post(backendUrl + '/api/auth/reset-password', 
+      {email, otp, newPassword})
+      data.success ? toast.success(data.message) : toast.error(data.message)
+      data.success && navigate('/login')
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
   return (
     <div className='flex items-center justify-center min-h-screen bg-gradient-to-br from white to bg-red-400'>
       <img onClick={()=>navigate('/')} src={assets.logo} alt="" className="absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer"/>
 
       {/* enter email id */}
-      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+
+      {!isEmailSent && 
+      <form onSubmit={onSubmitEmail} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
       <h1 className='text-white text-2xl font-semibold text-center mb-4'>
         Reset Password
       </h1>
@@ -52,9 +94,12 @@ const ResetPassword = () => {
           Submit
       </button>
       </form> 
+      } 
 
       {/* OTP input Form */}
-      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+
+      {!isOtpSubmitted && isEmailSent && 
+      <form onSubmit={onSubmitOTP} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
         <h1 className='text-white text-2xl font-semibold text-center mb-4'>
           Reset Password OTP
         </h1>
@@ -73,10 +118,11 @@ const ResetPassword = () => {
           Submit
         </button>
       </form>
+      }
 
       {/*enter new password*/}
-
-      <form className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
+      {isOtpSubmitted && isEmailSent && 
+      <form onSubmit = {onSubmitNewPassword} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
       <h1 className='text-white text-2xl font-semibold text-center mb-4'>
         New Password 
       </h1>
@@ -85,14 +131,14 @@ const ResetPassword = () => {
       </p>
       <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
         <img src={assets.lock_icon} alt="" className='w-3 h-3' />
-        <input type='email' placeholder='Email Address' className='bg-transparent outline-none text-white'
-        value={email} onChange={e => setEmail(e.target.value)} required/>
+        <input type='password' placeholder='Password' className='bg-transparent outline-none text-white'
+        value={newPassword} onChange={e => setNewPassword(e.target.value)} required/>
       </div>
       <button className='w-full py-2.5 bg-gradient-to-r from-indigo-500 to bg-indigo-900 text-white rounded-full mt-3'>
           Submit
       </button>
       </form> 
-
+    }
 
     </div>
   )
